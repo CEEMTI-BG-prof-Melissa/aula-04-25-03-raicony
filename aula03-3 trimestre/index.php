56e7f9d123350efd8ab194319faf3fab23c40cb1
raicony-ray <raicony.valentim@aluno.edu.es.gov.br>
2025-09-09 11:56:40 -0300

    Create index.php

---
+++ aula03-3 trimestre/index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title> Aula 03  PHP </title>
+    </head>
+    <body>
+        <?php
+            $nome1="Raicony";
+            $nome2="Raicony";
+            $n1=10;
+            $n2=2;
+            $soma=$n1+$n2;  
+
+            echo"Meu nome é " .$nome1;
+            echo"Meu nome é $nome1";
+            echo"<br> <br> A soma de $n1 com $n2 e igual a $soma";
+        ?>
+    </body>
+</html> 
