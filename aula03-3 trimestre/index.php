be0e7861cca56f2111d948dc01a73c7bf32b8e6f
raicony-ray <raicony.valentim@aluno.edu.es.gov.br>
2025-09-10 11:53:35 -0300

    index.php

--- aula03-3 trimestre/index.php
+++ aula03-3 trimestre/index.php
@@ -11,8 +11,11 @@
             $soma=$n1+$n2;  
 
             echo"Meu nome é " .$nome1;
-            echo"Meu nome é $nome1";
             echo"<br> <br> A soma de $n1 com $n2 e igual a $soma";
+            echo"<br> <br> O resultado da subtração de $n1 e $n2 é igual a: " .($n1-$n2) .".";
+            echo"<br> <br> A multiplicaçao dos numeros $n1 e $n2 é igual a: " .($n1*$n2) .".";
+            echo"<br> <br> a divisão dos numeros $n1 por $n2 é igual a: ".($n1/$n2) .".";
+            echo"<br> <br> O resto da divisão dos numeros $n1 por $n2 é igual a:".($n1%$n2) .".";
         ?>
     </body>
-</html> 
+</html>
