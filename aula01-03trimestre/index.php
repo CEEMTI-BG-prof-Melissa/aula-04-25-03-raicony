b269bc5ad99b3171a40e1acea990a853fb465dd7
raicony-ray <raicony.valentim@aluno.edu.es.gov.br>
2025-09-10 11:57:37 -0300

    index.php

---
+++ aula01-03trimestre/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title> Aula 01 PHP </title>
+    </head>
+    <body>
+        <?php
+            echo "Hello world";
+            echo "raicony";
+        ?>
+    </body>
+</html>
